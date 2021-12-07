b59806820426771719dd53a76d9b975f454a065e
Michael Latter <mlatter2@gmail.com>
2021-12-06 23:28:33 -0800

    proof read code, could be small error

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
 'use strict';
-//console.log('hello');
+
+//console.log('hello world!');
 
 let hours = ['6am', '7am', '8am', '9am', '10', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-//let ul =document.getElementById
-//('seattle');
-//console.log(ul);
+let ul =document.getElementById('seattle');
 
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min +1) +min);
+console.log(ul);
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//getRandomNumber();
+
+getRandomInt();
 
 let seattle = {
   name: 'Seattle',
@@ -19,11 +21,23 @@ let seattle = {
   avg: 6.3,
   cookiesPerHr: [], //per hour
   customerPerHr: [], //per hour
-  minMaxCust: function() {
-    for (let i =0; i < hours.length; i++) {
-      let x = getRandomInt(this.minCust,this.maxCust);
+  minMaxCust: function () {
+    for (let i = 0; i < hours.length; i++) {
+      let x = getRandomInt(this.minCust, this.maxCust);
       this.customerPerHr.push(x);
-  
+      console.log(this.customerPerHr);
     }
   }
 },
+
+  cookiesPerHr: function() {
+    for (let i = 0; i < hours.length; i++) {
+      let perhour = Math.ceil(this.customerPerHr[i] * this.avg);
+      this.cookiesPerHr.push(perhour);
+      console.log(perhour);
+
+  }
+};
+seattle.minMaxCust();
+seattle.cookiesPerHr();
+
