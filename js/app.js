997f6278c4b1dfba157ef49426d19551c9967190
Michael Latter <mlatter2@gmail.com>
2021-12-12 03:13:56 -0800

    need to add forms

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 'use strict';
 
 //console.log('helo world');
+let form = document.querySelector('form');
+
 
 let section = document.getElementById('seattle');
-console.log(section);
+
+let article = document.createElement('article');
+section.appendChild(article);
+
+let table = document.createElement('table');
+article.appendChild(table);
+let tfoot = document.createElement('tfoot');
 
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
@@ -17,6 +25,7 @@ function Store(name, minCustPerHour, maxCustPerhour, avgCookiesPerSale) {
   this.cookiesSalesPerHour = [];
   this.dailyTotal = 0;
   stores.push(this);
+  this.render();
 }
 
 Store.prototype.custPerHour = function () {
@@ -39,10 +48,10 @@ Store.prototype.render = function () {
 
   this.calcCookieSalesPerHour();
 
-  tr = document.createElement('tr');
+  let tr = document.createElement('tr');
   table.appendChild(tr);
 
-  th = document.createElement('th');
+  let th = document.createElement('th');
   // th.setAttribute('class', 'names');
   th.textContent = `${this.name}`;
   tr.appendChild(th);
@@ -58,60 +67,75 @@ Store.prototype.render = function () {
   tr.appendChild(td);
 };
 
-let seattle = new Store('Seattle', 23, 65, 6.3);
-let tokyo = new Store('Tokyo', 3, 24, 1.2);
-let dubai = new Store('Dubai', 11, 38, 3.7);
-let paris = new Store('Paris', 20, 38, 2.3);
-let lima = new Store('Lima', 2, 16, 4.6);
+function renderHeader() {
 
-// console.log(seattle);
-// console.log(seattle.avgSalesPerHour());
-// console.log(seattle.custPerHour());
-// console.log(seattle.calcCookiesSalesPerHour());
+  let thead = document.createElement('thead');
+  table.appendChild(thead);
 
-let article = document.createElement('article');
-section.appendChild(article);
+  let tr = document.createElement('tr');
+  thead.appendChild(tr);
 
-let table = document.createElement('table');
-article.appendChild(table);
-
-let thead = document.createElement('thead');
-table.appendChild(thead);
-
-let tr = document.createElement('tr');
-thead.appendChild(tr);
-
-let th = document.createElement('th');
-th.textContent = '';
-tr.appendChild(th);
-
-for (let i = 0; i < hours.length; i++) {
   let th = document.createElement('th');
-  th.textContent = `${hours[i]}`;
+  th.textContent = '';
+  tr.appendChild(th);
+
+  for (let i = 0; i < hours.length; i++) {
+    let th = document.createElement('th');
+    th.textContent = `${hours[i]}`;
+    tr.appendChild(th);
+  }
+
+  th = document.createElement('th');
+  // th = setAttribute('class', 'dlt');
+  th.textContent = 'Daily Store Total';
   tr.appendChild(th);
 }
 
-th = document.createElement('th');
-// th = setAttribute('class', 'dlt');
-th.textContent = 'Daily Store Total';
-tr.appendChild(th);
+function renderFooter() {
+  table.appendChild(tfoot);
+  let tr = document.createElement('tr');
+  tfoot.appendChild(tr);
+  let td = document.createElement('td');
+  td.textContent = 'Totals';
+  tr.appendChild(td);
+
+  td = document.createElement('td');
+
+  let dailyTotal = 0;
+  for (let i = 0; i < hours.length; i++) {
+    let td = document.createElement('td');
+    let hourlyTotals = 0;
+    for (let x = 0; x < stores.length; x++) {
+      hourlyTotals += stores[x].cookiesSalesPerHour[i];
+    }
+    // console.log(hourlyTotals);
+    td.textContent = hourlyTotals;
+    dailyTotal += hourlyTotals;
+    tr.appendChild(td);
+  }
+  td = document.createElement('td');
+  td.textContent = dailyTotal;
+  tr.appendChild(td);
+}
+
+//step 3 define event handler
+function handleSubmit(event){
+  event.preventDefault();
+  //insert code here
 
 
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
 
-tr = document.createElement('tr');
-table.appendChild(tr);
-let td = document.createElement('td');
-tr.appendChild(td);
+}
 
-td = document.createElement('td');
-// function dailyTotal() {
-//   let dailyTotal = 0;
-//   for (let i = 0; i < hours.length; i++) {
-//     let td = document.createElement('td');
-//     let hourlyTotals = 0;
-//     for (let x = 0; x < stores.length; x++) {
+new Store('Seattle', 23, 65, 6.3);
+new Store('Tokyo', 3, 24, 1.2);
+new Store('Dubai', 11, 38, 3.7);
+new Store('Paris', 20, 38, 2.3);
+new Store('Lima', 2, 16, 4.6);
+
+renderHeader();
+renderFooter();
+
+//event listener
+form.addEventListener('submit', handleSubmit);
+
